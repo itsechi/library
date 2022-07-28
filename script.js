14424b45ef84b437ec3dfd732e53935d80874638
itsechi <wardulinskao@gmail.com>
2022-07-28 13:01:28 +0200

    Fix html headings and markRead function to work with user input

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const title = document.querySelector('#book-title');
 const author = document.querySelector('#book-author');
 const pages = document.querySelector('#book-pages');
-const addBtn = document.querySelector('#add-book');
 const read = document.querySelector('#book-status');
+const addBtn = document.querySelector('form');
 const cardContainer = document.querySelector('.card-container');
 const openFormBtn = document.querySelector('#open-form');
 const form = document.querySelector('.form-modal');
 const overlay = document.querySelector('.form-overlay');
 let myLibrary = [];
-
 
 // show form
 openFormBtn.addEventListener('click', () => form.classList.remove('hidden'));
@@ -16,6 +15,11 @@ openFormBtn.addEventListener('click', () => form.classList.remove('hidden'));
 overlay.addEventListener('click', closeForm);
 function closeForm() {
   form.classList.add('hidden');
+  // clear input fields
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.checked = false;
 }
 
 // create a new book
@@ -25,9 +29,9 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
-
+ 
 // add book to myLibrary array
-addBtn.addEventListener('click', addBookToLibrary);
+addBtn.addEventListener('submit', addBookToLibrary);
 function addBookToLibrary(e) {
   e.preventDefault();
   const book = new Book(title.value, author.value, pages.value, read.checked);
@@ -42,19 +46,42 @@ function displayBooks() {
   myLibrary.forEach(book => {
     const displayBook = document.createElement('div');
     displayBook.className = 'card';
-    displayBook.innerHTML = `<h2 class="title">${book.title}</h2>
-    <h3 class="author">${book.author}</h3>
-    <h4 class="pages">${book.pages} pages</h4>
-    <h3 class="status">${
+    displayBook.innerHTML = `<h1 class="title">${book.title}</h1>
+    <h2 class="author">${book.author}</h2>
+    <h3 class="pages">${book.pages} pages</h3>
+    <h2 class="status">${
       book.read
         ? `<input type="checkbox" id="mark-read" checked>`
         : `<input type="checkbox" id="mark-read">`
-    } mark read</h3>
+    } mark read</h2>
     <div class="status-bar ${book.read ? `read` : 'not-read'}">`;
     cardContainer.appendChild(displayBook);
   });
-}
 
+  // mark read
+  const markReadBtn = document.querySelectorAll('#mark-read');
+  markReadBtn.forEach(mark =>
+    addEventListener('click', e => {
+      if (e.target == mark) {
+        if (!e.target.checked) {
+          e.target.parentNode.nextSibling.nextElementSibling.classList.remove(
+            'read'
+          );
+          e.target.parentNode.nextSibling.nextElementSibling.classList.add(
+            'not-read'
+          );
+        } else if (e.target.checked) {
+          e.target.parentNode.nextSibling.nextElementSibling.classList.remove(
+            'not-read'
+          );
+          e.target.parentNode.nextSibling.nextElementSibling.classList.add(
+            'read'
+          );
+        }
+      }
+    })
+  );
+}
 
 // test books
 const ISOLT = new Book('In Search of Lost Time', 'Marcel Proust', 4215, true);
@@ -62,21 +89,3 @@ myLibrary.push(ISOLT);
 const Hamlet = new Book('Hamlet', 'William Shakespeare', 104, false);
 myLibrary.push(Hamlet);
 displayBooks();
-
-// mark read
-const markReadBtn = document.querySelectorAll('#mark-read');
-markReadBtn.forEach(mark =>
-  addEventListener('click', e => {
-    if (e.target == mark) {
-      if (!e.target.checked) {
-        e.target.parentNode.nextSibling.nextElementSibling.classList.remove('read');
-        e.target.parentNode.nextSibling.nextElementSibling.classList.add('not-read');
-      } else if (e.target.checked) {
-        e.target.parentNode.nextSibling.nextElementSibling.classList.remove('not-read');
-        e.target.parentNode.nextSibling.nextElementSibling.classList.add('read');
-      }
-    }
-  })
-);
-
-
